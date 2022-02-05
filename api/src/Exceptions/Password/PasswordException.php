0efa8b48f4db354b5ef16da7b9bbc70b32bef509
jorgechavezrnd <jorgechavezrnd@gmail.com>
2022-02-05 17:13:45 -0400

    Add Encoder Service

---
+++ api/src/Exceptions/Password/PasswordException.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Exceptions\Password;
+
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
+
+class PasswordException extends BadRequestException
+{
+    public static function invalidLength(): self
+    {
+        throw new self('Password must be at least 6 characters');
+    }
+}
